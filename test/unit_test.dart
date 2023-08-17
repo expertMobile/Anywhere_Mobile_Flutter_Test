9542ed76ee4305177700abfebf8a1fb0253561f5
METAS Software <perfectsoft2019@gmail.com>
2023-08-17 15:23:09 -0600

    feat: Added tests files

---
+++ test/unit_test.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:metas_test/flavour_config.dart';
+import 'package:metas_test/modules/dashboard/apis/character_provider.dart';
+import 'package:metas_test/modules/dashboard/models/character_model.dart';
+import 'package:metas_test/modules/home/provider/home_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  testWidgets('fetches character list successfully',
+      (WidgetTester tester) async {
+    final charactersData = {
+      "RelatedTopics": [
+        {"name": "Character 1"},
+        {"name": "Character 2"},
+      ]
+    };
+
+    await tester.pumpWidget(
+      MaterialApp(
+        builder: EasyLoading.init(),
+        home: MultiProvider(
+          providers: [
+            ChangeNotifierProvider<HomeProvider>(
+              create: (context) => HomeProvider(
+                config: FlavourConfig(
+                    api:
+                        "http://api.duckduckgo.com/?q=simpsons+characters&format=json"),
+              ),
+            ),
+            ChangeNotifierProvider<CharacterProvider>(
+              create: (context) => CharacterProvider(),
+            ),
+          ],
+          child: Builder(
+            builder: (context) => FloatingActionButton(
+              onPressed: () =>
+                  Provider.of<CharacterProvider>(context, listen: false)
+                      .getCharacterList(context),
+              child: const Text('Fetch Characters'),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    // Simulate tapping the button
+    await tester.tap(find.text('Fetch Characters'));
+    await tester.pumpAndSettle();
+
+    // Get the current instance of HomeProvider
+    final characterProvider = Provider.of<CharacterProvider>(
+        tester.element(find.byType(FloatingActionButton)),
+        listen: false);
+
+    expect(characterProvider.characters, isA<List<CharacterModel>>());
+    expect(characterProvider.characters.length, 0);
+    // Add more assertions as needed
+  });
+}
